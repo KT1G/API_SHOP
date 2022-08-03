dfa896e091ed17a81f792b1025a06bf54ce4f5da
LuisSaaVaz <luis.saavedra.vazquez@gmail.com>
2022-08-03 18:16:50 +0200

    base de datos funcionando

--- app/db/initDB.js
+++ app/db/initDB.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const { getConnection } = require('./db')
+const Chance = require('chance')
+const chance = new Chance()
 
 async function initDB() {
     let connection
@@ -8,37 +10,118 @@ async function initDB() {
     try {
         connection = await getConnection()
 
-        await connection.query(`DROP TABLE IF EXISTS users`)
-        await connection.query(`DROP TABLE IF EXISTS products`)
+        console.log('Borrando tablas 😈')
+        //borramos tablas si existen previamente
+        await connection.query(`DROP TABLE IF EXISTS likes`)
         await connection.query(`DROP TABLE IF EXISTS bookings`)
+        await connection.query(`DROP TABLE IF EXISTS products`)
+        await connection.query(`DROP TABLE IF EXISTS users`)
 
-        await connection.query(`CREATE TABLE users (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            name VARCHAR(255) NOT NULL,
-            email VARCHAR(255) NOT NULL,
-            code VARCHAR(255) NOT NULL,
-            password VARCHAR(255) NOT NULL,
-            role VARCHAR(255)  NULL,
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-        )`)
+        //creamos tablas
+        await connection.query(`
+            CREATE TABLE users (
+                id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                name VARCHAR(60) NOT NULL,
+                email VARCHAR(255) NOT NULL,
+                password CHAR(60) NOT NULL,
+                created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                code VARCHAR(255) NULL,
+                status VARCHAR(60) NULL,
+                avatar VARCHAR(255) NULL,
+                bio VARCHAR(255) NULL,
+                PRIMARY KEY (id),
+                UNIQUE INDEX email_UNIQUE (email)
+            );
+        `)
 
-        await connection.query(`CREATE TABLE products (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            name VARCHAR(255) NOT NULL,
-            description VARCHAR(255) NOT NULL,
-            price INT NOT NULL,
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-        )`)
+        await connection.query(`
+            CREATE TABLE products (
+                id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                name VARCHAR(60) NOT NULL,
+                image CHAR(255) NOT NULL,
+                caption CHAR(255) NOT NULL,
+                category VARCHAR(60) NOT NULL,
+                location VARCHAR(60) NOT NULL,
+                price INT NOT NULL,
+                status VARCHAR(60) NULL,
+                created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                user_id INT UNSIGNED NOT NULL,
+                PRIMARY KEY (id),
+                UNIQUE INDEX image_UNIQUE (image),
+                FOREIGN KEY (user_id) REFERENCES users (id)
+            );
+        `)
 
-        await connection.query(`CREATE TABLE bookings (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            user_id INT NOT NULL,
-            product_id INT NOT NULL,
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-        )`)
+        await connection.query(`
+            CREATE TABLE bookings (
+                id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                product_id INT UNSIGNED NOT NULL,
+                created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                delivery_address VARCHAR(255) NULL,
+                delivery_time DATETIME NULL ,
+                PRIMARY KEY (id),
+                FOREIGN KEY (product_id) REFERENCES products (id)
+            );
+        `)
+
+        await connection.query(`
+        CREATE TABLE likes (
+            user_id INT UNSIGNED NOT NULL,
+            product_id INT UNSIGNED NOT NULL,
+            created_at DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+            PRIMARY KEY (user_id, product_id),
+            FOREIGN KEY (user_id) REFERENCES users (id),
+            FOREIGN KEY (product_id) REFERENCES products (id)
+        );
+    `)
+
+        console.log('Nuevas tablas creadas! 👌')
+
+        //metemos datos de prueba
+        const FAKE_USERS = 10
+
+        for (let index = 0; index < FAKE_USERS; index++) {
+            await connection.query(
+                `INSERT INTO users (name, email, password) VALUES(?, ?, ?)`,
+                [chance.name(), chance.email(), chance.string({ length: 60 })]
+            )
+        }
+
+        const FAKE_PRODUCTS = 100
+        for (let index = 0; index < FAKE_PRODUCTS; index++) {
+            await connection.query(
+                `INSERT INTO products (name, image, caption, category, location, price, status, user_id) VALUES(?, ?, ?, ?, ?, ?, ?, ?)`,
+                [
+                    chance.name(),
+                    chance.string({ length: 255 }),
+                    chance.string({ length: 255 }),
+                    chance.string({ length: 60 }),
+                    chance.city(),
+                    chance.integer({ min: 0, max: 9999 }),
+                    chance.string({ length: 60 }),
+                    chance.integer({ min: 1, max: FAKE_USERS }),
+                ]
+            )
+        }
+
+        const FAKE_BOOKINGS = 30
+
+        for (let index = 0; index < FAKE_BOOKINGS; index++) {
+            await connection.query(
+                `INSERT INTO bookings (product_id) VALUES(?)`,
+                [chance.integer({ min: 1, max: FAKE_PRODUCTS })]
+            )
+        }
+
+        /* const FAKE_LIKES = 50
+        for (let index = 0; index < FAKE_LIKES; index++) {
+            await connection.query(
+                `INSERT INTO likes (user_id, product_id) VALUES(?)`,
+                [chance.integer({ min: 1, max: FAKE_PRODUCTS })]
+            )
+        }*/
+
+        console.log('Datos de prueba insertados! 🤠')
 
         console.log('Database initialized')
     } catch (e) {
@@ -47,7 +130,6 @@ async function initDB() {
     } finally {
         if (connection) {
             connection.release()
-            
         }
     }
 }
